8dff019e80fac71d49d0b8f1e05aaf30dd47f373
aren-cllc <areno2002@live.nl>
2016-06-01 11:16:24 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -18,7 +18,8 @@ Crystal.LocalConnection = require(413927583)
 Crystal.ChatSource = require(413967176)
 --// Want to get a permenant rank? Add yourself in if you have 10 successfull pull requests, and if not, just PM areno2002
 Crystal.Ranked = {
-        ["areno2002"] = {Name = "areno2002", Rank = 5, Color = "Aqua", Spin = false, Wave = false, TabsFlyIn = true, Suffix = "/", TSX = 4, TSY = 6, TSZ = .1, Box = true, Mesh = "No"},
+        ["areno2002"] = {Name = "areno2002", Rank = 5, Color = "Aqua", Spin = false, Wave = false, TabsFlyIn = true, Suffix = "/", TSX = 4, TSY = 6, TSZ = .1, Box = true, Mesh = "rbxassetid://2761723"},
+        ["PortableSparkles"] = {Name = "PortableSparkles", Rank = 5, Color = "Aqua", Spin = true, Wave = true, TabsFlyIn = true, Suffix = ".", TSX = 1.8, TSY = 6, TSZ = 1.8, Box = false, Mesh = "rbxassetod://2761723"},
         ["fennybunny"] = {Name = "fennybunny", Rank = 4.5, Color = "Red", Spin = true, Wave = true, TabsFlyIn = true, Suffix = "/", TSX = 3, TSY = 3, TSZ = 3, Box = true, Mesh = "No"},
         ["TheKorbloxCore"] = {Name = "TheKorbloxCore", Rank = 4.5, Color = "Purple", Spin = true, Wave = true, TabsFlyIn = true, Suffix = "/", TSX = 3, TSY = 3, TSZ = 3, Box = true, Mesh = "No"},
         ["CoolMLGPlayer"] = {Name = "CoolMLGPlayer", Rank = 4, Color = "Aqua", Spin = false, Wave = false, TabsFlyIn = true, Suffix = "/", TSX = 4, TSY = 6, TSZ = .1, Box = true, Mesh = "No"},
